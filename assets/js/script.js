ef355044d1e5f5e1dd16de43ab821be0adcedac9
John Helder <johnhelderjp@gmail.com>
2022-12-22 20:43:13 -0300

    Movendo o ponteiro dos segundos

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ function updateClock(){
   digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 
   let sDeg = ((360 / 60) * second) - 90;
+
+  sElement.style.transform = `rotate(${sDeg}deg)`;
 }
 
 function fixZero(time){
